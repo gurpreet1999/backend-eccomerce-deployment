e5bc35dc2affae7ed7505bde988ee29edb85237a
gurpreet1999 <127664291+gurpreet1999@users.noreply.github.com>
2023-08-19 11:24:35 +0530

    ok

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cors=require("cors")
 
 
 app.use(cors({
-    origin:'http://localhost:3000', 
+    origin:'*', 
     credentials:true,           
     optionSuccessStatus:200
 }))
--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,7 +6,8 @@ const token=user.getJWTToken()
 
 const options={
     expires:new Date(Date.now()+ 15*24*60*60*1000),
-    httpOnly: true
+    httpOnly: true,
+    SameSite:None
     
 }
 
