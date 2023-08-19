64d819f7dc5478b29941dfbf2537a8120314dd8d
gurpreet1999 <127664291+gurpreet1999@users.noreply.github.com>
2023-08-19 11:51:49 +0530

    ok

--- app.js
+++ app.js
@@ -23,7 +23,12 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended:true
 }))
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 
 
